d476fefa8850b92c27db7c222d68a2beaac8b36e
Gabriel Filion <gabster@lelutin.ca>
2021-04-17 00:25:31 -0400

    Create tests for local hosts database
    The hosts format is not very complex so we can have a handful of tests
    that should cover almost everything.
    
    We don't want to match partial host names: only the ones that fully
    match will be looked up in that database.
    
    Since this is the first test we also need to add pytest and pytest-mock
    (used in the tests) to the requirements.txt file.

---
+++ tests/test_hosts.py
@@ -0,0 +1,24 @@
+"""Test features related to /etc/hosts."""
+from dns_deep_state import hosts
+import pytest
+
+
+hosts_file = """127.0.0.1 localhost
+
+192.168.10.12 remote remote.domain # this should still match
+192.158.10.25 hostname.fqdn"""
+
+
+@pytest.mark.parametrize("hostname,result",
+                         [("hostname.fqdn", True), ("remote", True),
+                          ("rem", False), ("nope", False),
+                          ("192.158.10.25", False)])
+def test_hostname_found(mocker, hostname, result):
+    """We're searching for a hostname which is present in the database."""
+    m = mocker.patch('builtins.open', mocker.mock_open(read_data=hosts_file))
+
+    h = hosts.Hosts()
+    present = h.in_database(hostname)
+
+    m.assert_called_once_with("/etc/hosts", "r")
+    assert present is result
