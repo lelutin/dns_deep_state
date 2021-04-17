3f6c05a7814f495aa3dbad1cb65e1dba2b91cd01
Gabriel Filion <gabster@lelutin.ca>
2021-04-17 00:34:42 -0400

    Implement finding host names in hosts database.

---
+++ src/dns_deep_state/hosts.py
@@ -0,0 +1,39 @@
+"""Perform some verifications on the local hosts database."""
+import re
+
+
+class Hosts:
+    """Test for presence of a hostname inside the local hosts file.
+
+    This file is usually found in /etc/hosts in unix/linux systems.
+
+    The presence of a hostname inside this file might drastically change the
+    behavior of a service that you're trying to use on a certain hostname, so
+    it's important to check whether you have such an override in place.
+    """
+
+    def __init__(self, hosts_database="/etc/hosts"):
+        """Prepare all relevant drivers for queries."""
+        self.hosts_database = hosts_database
+
+    def in_database(self, hostname):
+        """Check whether a hostname is present in the local hosts database."""
+        with open(self.hosts_database, 'r') as hosts:
+            for line in hosts.readlines():
+                # remove trailing newline char
+                line = re.sub(r'\n$', '', line)
+                # chop off comments
+                line = re.sub(r' *#.*$', '', line)
+                # empty up lines that have only spaces or tabs; they're not
+                # interesting to process
+                line = re.sub(r'^[ \t]+$', '', line)
+                # discard empty lines
+                if not line:
+                    continue
+
+                host_aliases = line.split()[1:]
+
+                if hostname in host_aliases:
+                    return True
+
+        return False
