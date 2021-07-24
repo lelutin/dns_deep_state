4d7d58c5497f1535cb567a7b2ef7f87fdac55a89
Gabriel Filion <gabster@lelutin.ca>
2021-07-24 19:37:14 -0400

    mock out unnecessary probes during report tests
    we're now starting to see a bit more of the previously encountered
    effect: whenever we're instantiating a report class, all probes also get
    instantiated and thus they add some unwanted external calls.
    
    with the new RDAP probe, it means that we're bootstrapping the RDAP
    library on every report test! that has to go away for speed and sanity.

--- tests/test_report.py
+++ tests/test_report.py
@@ -15,11 +15,36 @@ from dns_deep_state.report import DomainReport
 from .test_hosts import hosts_file
 
 
-def test_report_known_tld():
+def domain_report_mocked_probes(mocker, probe_used=None):
+    """Instantiate DomainReport and mock out probes.
+
+    If probe_used is not None, then one probe will be left untouched.
+
+    Most probes tend to initialize some connection, or read a file in order to
+    be ready to query something for information. When we're testing out
+    individual reports, we don't need to initialize the probes that are not
+    checked on every test. Also this initialization step can add up to consume
+    quite a lot of time. We'll only test one desired probe at a time to keep
+    the tests focused and quicker.
+    """
+    probes = {
+        "psl": "dns_deep_state.report.PublicSuffixList",
+        "registry": "dns_deep_state.report.RegistryProbe",
+        "dns": "dns_deep_state.report.DnsProbe",
+        "hosts": "dns_deep_state.report.HostsProbe",
+    }
+
+    for name, func in probes.items():
+        if name != probe_used:
+            # Mock out any probe that wasn't requested for testing
+            mocker.patch(func, mocker.MagicMock)
+
+    return DomainReport()
+
+
+def test_report_known_tld(mocker):
     """Checking a domain that uses one of the known "public suffixes"."""
-    reporter = DomainReport()
-    # TODO stub out calls to full_report on individual probes since that's not
-    # what we want to test here
+    reporter = domain_report_mocked_probes(mocker, probe_used="psl")
 
     r = json.loads(reporter.full_report("example.com"))
     assert r["domain"] == "example.com"
@@ -28,25 +53,25 @@ def test_report_known_tld():
     assert r2["domain"] == "example.com"
 
 
-def test_constructor_unknown_tld():
+def test_constructor_unknown_tld(mocker):
     """Checking a domain that doesn't have one of the "public suffixes"."""
-    reporter = DomainReport()
+    reporter = domain_report_mocked_probes(mocker, probe_used="psl")
 
     with pytest.raises(ValueError):
         reporter.full_report("blah.patate")
 
 
 def test_local_hosts_report(mocker):
-    """Grab a report for a series of hosts and see that it matches expectations."""
+    """Check presence in local hosts for a series of hosts.
+
+    We want to see whether requesting multiple informations at once functions
+    properly. That's why we're not parametrizing fixtures and checking them one
+    at a time.
+    """
     # We need this mock before initializing the reporter, otherwise the call to
     # the real open() will happen during instantiation
     m = mocker.patch('builtins.open', mocker.mock_open(read_data=hosts_file))
-    # We're not testing other resolvers so we want to avoid instantiating them
-    mocker.patch("dns_deep_state.report.PublicSuffixList", mocker.MagicMock)
-    mocker.patch("dns_deep_state.report.DnsProbe", mocker.MagicMock)
-    mocker.patch("dns_deep_state.report.RegistryProbe", mocker.MagicMock)
-
-    reporter = DomainReport()
+    reporter = domain_report_mocked_probes(mocker, probe_used="hosts")
     m.assert_called_once_with("/etc/hosts", "r")
 
     h_list = ["hostname.fqdn", "remote", "rem", "nope", "192.158.10.25"]
