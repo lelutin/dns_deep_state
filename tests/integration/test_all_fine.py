32e736c21033c85741aaae69d8e036c0abbc02b0
Gabriel Filion <gabster@lelutin.ca>
2021-08-26 23:04:02 -0400

    Create the first integration test
    This new test performs the real lookups on live systems, so we'll be
    able to see if the unit tests have wrong abstractions or if some
    elements are still missing.
    
    .. and we're already discovering a problem with this!

---
+++ tests/integration/test_all_fine.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Make a live run with the library with a domain that's working nicely.
+
+Hopefully we can get information from all systems as part of the report for
+this test.
+"""
+
+from dns_deep_state.report import DomainReport
+
+
+def test_live_full_report():
+    """Get information from example.com from live systems."""
+    dr = DomainReport()
+
+    report = dr.full_report("example.com")
+
+    # Nothing super useful to check for now (but there will be once the
+    # structure of the report starts solidifying).
+    # The instructions above at least test that we're not getting unexpected
+    # errrors while running, and we can see what the report looks like:
+    print(report)
